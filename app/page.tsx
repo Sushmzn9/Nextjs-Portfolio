28894deed1a317bcd3447fa2ed4edb00699fd515
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-10-22 14:27:32 +1100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import WorkExperience from "@/Components/WorkExperience";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scroll-smooth">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y  snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scroll-smooth">
       <Header />
       <section id="home" className="snap-center">
         <Hero />
